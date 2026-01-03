fc1df05284c7bc625f4d73c44164f308a36f2885
Subhan Zaheer <xheksubhan.zaheer@gmail.com>
2026-01-04 03:08:37 +0500

    Add files via upload

---
+++ JavaScript Arrays/indxe.js
@@ -0,0 +1,30 @@
+let arr = [1 , 2 , 4 , 5 , 7]
+    // index :0   1   2   3   4
+    
+    arr[0] = 5666;
+
+    // console.log( arr , typeof arr);
+    // console.log(arr);
+    // console.log(arr.length);
+    // console.log(arr[0])
+    // console.log(arr[2])
+
+    console.log(arr.toString());
+    console.log(arr.join(" and "));
+
+    a = [1 , 2 , 3 , 4 , 5 ,6]
+    console.log(a.pop());
+    console.log(a);
+    
+    console.log(a.push(100));
+    console.log(a.push("Subhan"));
+    console.log(a.shift(100));
+    console.log(a.unshift("Jack"));
+    console.log(delete a[5])
+    
+
+    let numbers = [1 , 2 , 3 , 4 , 5]
+        // index   0   1   2   3   4 
+    // numbers.splice(1 , 2)
+    numbers.slice(1 , 2)
+    
